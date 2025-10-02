6b6cac5b0729885193e1dbcc78541fb1c36d1be1
Lyndsay R <richardslyndsay@gmail.com>
2025-10-02 01:57:32 +0000

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,63 @@
-import rigoImageUrl from "../assets/img/rigo-baby.jpg";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 
 export const Home = () => {
+  const { store, dispatch } = useGlobalReducer();
 
-  const {store, dispatch} =useGlobalReducer()
+  function fetchItems(nature) {
+    fetch(`${store.BASE_API_URL}/${nature}`)
+      .then((response) => response.json())
+      .then((body) => {
+        dispatch({
+          type: "set_items",
+          payload: {
+            nature: nature,
+            items: body.results,
+            
+          },
+        });
+      });
+  }
+ 
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-		</div>
-	);
-}; 
+  useEffect(() => {
+    fetchItems("people");
+    fetchItems("planets");
+    fetchItems("vehicles");
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="d-flex flex-column w-100">
+          <h2>people</h2>
+          <div className="d-flex flex-nowrap items-container">
+            {store.people.map((people) => {
+              return (
+                <div className="card" style={{ width: "18rem" }}>
+                  <img src="..." className="card-img-top" alt="..." />
+                  <div className="card-body">
+                    <h5 className="card-title">{people.name}</h5>
+                    <p className="card-text">status: {people.status}</p>
+                    <p className="card-text">species: {people.species}</p>
+                    <p className="card-text">gender: {people.gender}</p>
+                    <Link
+                      to={`/people/${people.id}`}
+                      className="btn btn-primary"
+                    >
+                      Learn More
+                    </Link>
+                  </div>
+                </div>
+              );
+            })}
+            {/* {store.people.map((people) => {
+            return <ItemCard Item={people} nature="people" />;
+          })} */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/store.js
+++ src/store.js
@@ -1,32 +1,41 @@
-export const initialStore=()=>{
-  return{
-    message: null,
-    todos: [
-      {
-        id: 1,
-        title: "Make the bed",
-        background: null,
-      },
-      {
-        id: 2,
-        title: "Do my homework",
-        background: null,
-      }
-    ]
-  }
-}
+export const initialStore = () => {
+  return {
+    people: [
+      "https://www.swapi.tech/api/people"
+    ],
+    planets: ["https://www.swapi.tech/api/planets"],
+    vehicles: ["https://www.swapi.tech/api/vehicles"],
+    favorites: [],
+    BASE_API_URL: "https://www.swapi.tech/api",
+  };
+};
 
 export default function storeReducer(store, action = {}) {
-  switch(action.type){
-    case 'add_task':
+  switch (action.type) {
+    case "set_items": {
+      const nature = action.payload.nature;
+      const items = action.payload.items;
+      const newStore = { ...store };
+      if (nature === "people") {
+        newStore.people = items;
+      } else if (nature === "planets") {
+        newStore.planets = items;
+      } else if (nature === "vehicles") {
+        newStore.vehicles = items;
+      }
+      return newStore;
+    }
+    // case "add_task":
+    //   const { id, color } = action.payload;
 
-      const { id,  color } = action.payload
-
-      return {
-        ...store,
-        todos: store.todos.map((todo) => (todo.id === id ? { ...todo, background: color } : todo))
-      };
+    //   return {
+    //     ...store,
+    //     todos: store.todos.map((todo) =>
+    //       todo.id === id ? { ...todo, background: color } : todo
+    //     ),
+    //   };
     default:
-      throw Error('Unknown action.');
-  }    
+      throw Error("Unknown action.");
+  }
+  return store;
 }
